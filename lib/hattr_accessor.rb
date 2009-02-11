4689f1460c2410fbb2eb79b1b2e73d27e7f555fb
Kyle Crum <kyle.e.crum@gmail.com>
2009-02-11 00:02:10 -0200

    refactored code so that there are more hooks in which to override or alias_method_chain should the programmer choose to do so

--- lib/hattr_accessor.rb
+++ lib/hattr_accessor.rb
@@ -2,59 +2,75 @@ module Huberry
   module HattrAccessor
     class MissingAttributeError < StandardError; self; end
     
-    def hattr_accessor(*attrs)
-      options = attrs.last.is_a?(Hash) ? attrs.pop : {}
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+    
+    module ClassMethods
+    
+      def hattr_accessor(*attrs)
+        options = attrs.last.is_a?(Hash) ? attrs.pop : {}
       
-      raise MissingAttributeError, 'Must specify the :attribute option with the name of an attribute which will store the hash' if options[:attribute].nil?
-
-      attrs.each do |name|
+        raise MissingAttributeError, 'Must specify the :attribute option with the name of an attribute which will store the hash' if options[:attribute].nil?
+      
         # Defines a type casting getter method for each attribute
         #
-        define_method name do
-          value = send(options[:attribute])[name]
-          case options[:type]
-            when :string
-              value.to_s
-            when :integer
-              value.to_i
-            when :float
-              value.to_f
-            when :boolean
-              ![false, nil, 0, '0', ''].include?(value)
-            else
-              value
+        attrs.each do |name|
+          setter = (name.to_s + '=').to_sym
+          define_method(name) {self.get_hattr_attribute_value(options[:attribute],name, options[:type])}
+          define_method(setter) {|val| self.set_hattr_attribute_value(options[:attribute],name, val, options[:type])}
+        end
+      
+        unless instance_methods.include?(options[:attribute].to_s)
+          define_method(options[:attribute]) do
+            instance_variable_set("@#{options[:attribute]}".to_sym,{}) unless instance_variable_get("@#{options[:attribute]}".to_sym).kind_of?(Hash)
+            instance_variable_get("@#{options[:attribute]}".to_sym)
           end
         end
-        
-        # Defines a setter method for each attribute
+
+        # Create the writer for #{options[:attribute]} unless it exists already
         #
-        define_method "#{name}=" do |value|
-          send(options[:attribute])[name] = value
-        end
+        attr_writer options[:attribute] unless instance_methods.include?("#{options[:attribute]}=")
+      
       end
       
-      # Create the reader for #{options[:attribute]} unless it exists already
-      #
-      attr_reader options[:attribute] unless instance_methods.include?(options[:attribute].to_s)
-      
-      # Create the writer for #{options[:attribute]} unless it exists already
-      #
-      attr_writer options[:attribute] unless instance_methods.include?("#{options[:attribute]}=")
-      
-      # Overwrites the method passed as the :attribute option to ensure that it is a hash by default
-      #
-      unless instance_methods.include?("#{options[:attribute]}_with_hattr_accessor")
-        class_eval <<-EOF
-          def #{options[:attribute]}_with_hattr_accessor
-            self.#{options[:attribute]} = {} if #{options[:attribute]}_without_hattr_accessor.nil?
-            #{options[:attribute]}_without_hattr_accessor
-          end
-          alias_method :#{options[:attribute]}_without_hattr_accessor, :#{options[:attribute]}
-          alias_method :#{options[:attribute]}, :#{options[:attribute]}_with_hattr_accessor
-        EOF
+    end
+    
+    protected
+
+    def set_hattr_attribute_value(hash_attribute,attribute, val, type)
+      converted_val = convert_to_type(val, type)
+      update_hash_attribute_value(hash_attribute,attribute,converted_val)
+    end
+
+    def get_hattr_attribute_value(hash_attribute,attribute, type)
+      convert_to_type(get_hashed_attribute(hash_attribute)[attribute], type)
+    end
+    
+    def convert_to_type(value, type)
+      case type
+        when :string
+          value.to_s
+        when :integer
+          value.to_i
+        when :float
+          value.to_f
+        when :boolean
+          ![false, nil, 0, '0', ''].include?(value)
+        else
+          value
       end
     end
+    
+    def get_hashed_attribute(hash_attribute)
+      send("#{hash_attribute}".to_sym)
+    end
+    
+    def update_hash_attribute_value(hash_attribute,attribute,val)
+      get_hashed_attribute(hash_attribute)[attribute] = val
+    end
+    
   end
 end
 
-Class.send :include, Huberry::HattrAccessor
+Object.send :include, Huberry::HattrAccessor
